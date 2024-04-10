f4b0f27dba0a671d6b4ffa5046b553d22350f2e4
Luiz Felipe <luizfelipe01700@gmail.com>
2024-04-09 21:43:04 -0300

    problemas com o back do form

--- js/confirmForm.js
+++ js/confirmForm.js
@@ -1,36 +1,46 @@
 function verifyForm(e, form){
     e.preventDefault();
 
-    let name = getElementById("name").value;
-    let email = getElementById("email").value;
-    let subject = getElementById("subject").value;
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let subject = document.getElementById("subject").value;
+    let errName = document.getElementById("errName");
+    let errEmail = document.getElementById("errEmail");
+    let errSubject = document.getElementById("errSubject");
 
-    let emailOk, nameOk, subjectOk = 0;
+    let emailOk = 0;
+    let  nameOk = 0;
+    let subjectOk = 0;
 
-    if(empty(email)){
-        window.alert("Escreva o seu e-mail ...");
-    } else {
-        if(email.includes("@")){
-            emailOk = 1;
-        } else {
-            window.alert("Coloque o @ no e-mail ...");
-        }
-    }
+    errName.textContent = "";
+    errEmail.textContent = "";
+    errSubject.textContent = "";
 
-    if(empty(name)){
-        window.alert("Escreva o seu nome ...");
+    if(name == ""){
+        errName.textContent = "Escreva o seu nome ...";
+        console.log("Escreva o seu nome ...");
     } else {
         nameOk = 1;
     }
 
-    if(empty(subject)){
-        window.alert("Escreva o assunto do contato ...");
+    if(email == ""){
+        errEmail.textContent = "Coloque o seu e-mail ...";
+    } else {
+        if(email.includes("@")){
+            emailOk = 1;
+        } else {
+            errEmail.textContent = "Coloque o @ no e-mail ...";
+        }
+    }
+
+    if(subject == ""){
+        errSubject.textContent = "Escreva o assunto do contato ...";
     } else {
         subjectOk = 1;
     }
 
     if(nameOk == 1 && emailOk == 1 && subjectOk == 1){
+        window.alert("E-mail enviado")
         form.submit();
     }
-
 }
